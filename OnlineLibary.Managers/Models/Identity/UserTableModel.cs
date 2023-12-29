76e77d41fd98d3abea1b3582dd23142efa5f9cf1
Jelizaveta <jz21055@edu.lu.lv>
2023-12-30 00:31:55 +0200

    Fix typo

--- OnlineLibary.Managers/Models/Identity/UserTableModel.cs
+++ OnlineLibary.Managers/Models/Identity/UserTableModel.cs
@@ -6,7 +6,6 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
-        public bool
         public DateTime DateCreated { get; set; }
     }
 }
